081b19d9a597ddd28e1b82fb083e94c080586572
mezitta <mezitta@mtu.edu>
2021-08-19 19:53:19 -0400

    Added endpoints for recipe list and recipe by id

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import waitOn from "wait-on";
 
 const app = express();
-const port = 3000;
+const port: number = 3000;
 
 console.log("Server is starting");
 waitOn({ resources: ["tcp:mysql:3306"] })
   .then(() => {
     app.get("/", (req, res) => res.send("Hello, world!"));
+    app.get("/recipes", (req, res) => res.send("List of all recipes :)"));
+    app.get("/recipes/:recipeId", (req, res) => res.send(req.params.recipeId));
     app.listen(port, () => {
       return console.log(`Server is listening on port ${port}`);
     });
